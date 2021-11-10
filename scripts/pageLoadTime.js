b26d799e36800e5ddec56cbc07cc616f4ee6558c
abdugaffar <abdugaffarkutbiddinov@gmail.com>
2021-11-10 10:17:07 +0300

    added pageLoadtime

--- scripts/pageLoadTime.js
+++ scripts/pageLoadTime.js
@@ -1,5 +1,5 @@
 const findLoadTime = (function() {
-    document.getElementsByClassName("load_time")[0].innerHTML = 'Page Load Time: ' +
+    document.getElementsByClassName("load_time")[0].innerHTML = 'Page Load Time is: ' +
       (window.performance.timing.domContentLoadedEventEnd -
         window.performance.timing.navigationStart) + ' ms';
   });
