a49109e8748d30dcd827065b07685e9171e2baeb
abdugaffar <abdugaffarkutbiddinov@gmail.com>
2021-11-23 14:12:23 +0300

    adding new todos and styling

---
+++ scripts/todo.js
@@ -0,0 +1,35 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+
+// Event Listeners
+todoButton.addEventListener('click',addTodo);
+
+
+// Functions 
+function addTodo(event) {
+    event.preventDefault();
+    // create div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    // create check button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // create delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(deleteButton);
+    // append todoList
+    todoList.appendChild(todoDiv);
+
+}
