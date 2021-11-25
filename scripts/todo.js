86901ec571cf8ca79b483a4a333b8ba46411dc2b
abdugaffar <abdugaffarkutbiddinov@gmail.com>
2021-11-25 21:31:41 +0300

    listeners added to checkbox and delete

--- scripts/todo.js
+++ scripts/todo.js
@@ -16,27 +16,21 @@ todoForm.addEventListener('submit', function(event) {
   addTodo(todoInput.value); // call addTodo function with input box current value
 });
 
-// function to add todo
 function addTodo(item) {
-  // if item is not empty
   if (item !== '') {
-    // make a todo object, which has id, name, and completed properties
     const todo = {
       id: Date.now(),
       name: item,
       completed: false
     };
 
-    // then add it to todos array
     todos.push(todo);
-    addToLocalStorage(todos); // then store it in localStorage
+    addToLocalStorage(todos); 
 
-    // finally clear the input box value
     todoInput.value = '';
   }
 }
 
-// function to render given todos to screen
 function renderTodos(todos) {
   todoItemsList.innerHTML = '';
 
@@ -53,41 +47,42 @@ function renderTodos(todos) {
 
     }
     todoName.innerHTML = item.name;
-    // li.innerHTML = `
-    //   <input type="checkbox" class="checkbox" ${checked}>
-    //   ${item.name}
-    //   <button class="delete-button">X</button>
-    // `;
+    const checkbox = clone.querySelector('.checkbox');
+    checkbox.addEventListener('click', (event) => {
+      toggle(event.target.parentElement.getAttribute('data-key'));
+    }
+      );
+    const del = clone.querySelector('.delete-button');
+    del.addEventListener('click', (event) => {
+        deleteTodo(event.target.parentElement.getAttribute('data-key'));
+      }
+        );
+      
     todoItemsList.appendChild(clone);
   });
 
 }
 
-// function to add todos to local storage
+
 function addToLocalStorage(todos) {
-  // conver the array to string then store it.
   localStorage.setItem('todos', JSON.stringify(todos));
-  // render them to screen
   renderTodos(todos);
 }
 
-// function helps to get everything from local storage
+
 function getFromLocalStorage() {
   const reference = localStorage.getItem('todos');
-  // if reference exists
+
   if (reference) {
-    // converts back to array and store it in todos array
     todos = JSON.parse(reference);
     renderTodos(todos);
   }
 }
 
-// toggle the value to completed and not completed
+
 function toggle(id) {
   todos.forEach(function(item) {
-    // use == not ===, because here types are different. One is number and other is string
     if (item.id == id) {
-      // toggle the value
       item.completed = !item.completed;
     }
   });
@@ -95,32 +90,14 @@ function toggle(id) {
   addToLocalStorage(todos);
 }
 
-// deletes a todo from todos array, then updates localstorage and renders updated list to screen
 function deleteTodo(id) {
-  // filters out the <li> with the id and updates the todos array
   todos = todos.filter(function(item) {
-    // use != not !==, because here types are different. One is number and other is string
+
     return item.id != id;
   });
 
-  // update the localStorage
   addToLocalStorage(todos);
 }
 
-// initially get everything from localStorage
 getFromLocalStorage();
-
-// after that addEventListener <ul> with class=todoItems. Because we need to listen for click event in all delete-button and checkbox
-todoItemsList.addEventListener('click', function(event) {
-  // check if the event is on checkbox
-  if (event.target.type === 'checkbox') {
-    // toggle the state
-    toggle(event.target.parentElement.getAttribute('data-key'));
-  }
-
-  // check if that is a delete-button
-  if (event.target.classList.contains('delete-button')) {
-    // get id from data-key attribute's value of parent <li> where the delete-button is present
-    deleteTodo(event.target.parentElement.getAttribute('data-key'));
-  }
-});
+const clone = template.content.cloneNode(true);
